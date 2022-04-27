f2d48985207e38d75b47606e44403ab2e11e4b59
jasu62 <jasu62@mailbox.org>
2022-04-26 22:02:57 -0500

    Implemented file metadata capture functionality.
    When the user POSTs a file to /api/fileanalyse, with the help of multer,
    the app stores the file on the server, gets the file metadata from the
    request object, responds to the user with a JSON object containing said
    metadata, then unlinks the file from disk.
    
    Comments were also rewritten to match my own style.
    sample.env is now simply .env, which is not tracked.
    .gitignore ignores .DS_Store files on Mac.

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
-var app = express();
-
+// Enable cross-origin resource sharing
+// so freeCodeCamp can test the app remotely
+const cors = require('cors');
 app.use(cors());
+
+// multer: For handling files.
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+
+// fs: For deleting files from disk with fs.unlink()
+const fs = require('fs');
+
+
+// Declare location of static files (CSS, JavaScript, images)
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
+// Set app's main page
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+// When the user POSTs a file to /api/fileanalyse,
+// the app will send back JSON with information about the file:
+// { name, type, size (in bytes) }
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const returnJSON = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+  console.log('Received upload:');
+  console.log(returnJSON);
+  res.json(returnJSON);
+  
+  fs.unlink(req.file.path, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(`${req.file.path} was deleted.`);
+    }
+  });
+});
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+// Start the server and listen for requests
+const port = ('PORT' in process.env) ? process.env.PORT : 3000;
+app.listen(port, () => {
+  console.log(`The app is listening on port ${port}.`);
 });
